67a917a4adbd38617b25d84141695cac666493c2
ashish torane <toraneashish7@gmail.com>
2020-12-29 22:54:56 +0530

    second commit

--- pages/views.py
+++ pages/views.py
@@ -6,3 +6,17 @@ def home(req):
 
 
     return render(req,'pages/home.html')
+
+def about(req):
+
+
+    return render(req,'pages/about.html')
+
+def services(req):
+
+
+    return render(req,'pages/services.html')
+def contact(req):
+
+
+    return render(req,'pages/contact.html')
